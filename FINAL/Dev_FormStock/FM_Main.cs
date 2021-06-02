1a131e401792db38231e46d1f797c2e6e1c58b39
Eunheui Kim <dmsgml110712@gmail.com>
2021-06-02 10:08:28 +0900

    종우+은희

--- FINAL/Dev_FormStock/FM_Main.cs
+++ FINAL/Dev_FormStock/FM_Main.cs
@@ -99,8 +99,7 @@ namespace Dev_FormStock
             }
             MyTabControl1.AddForm(ShowForm);
         }
-
-        
+               
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
